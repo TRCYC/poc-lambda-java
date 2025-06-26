a56d08f287f27aa2e82943290e996de458379125
Pankajrcyc <pankaj.gupta@rcyc.com>
2025-06-25 23:47:47 -0400

    Update LambdaHandler.java

--- src/main/com/rcyc/LambdaHandler.java
+++ src/main/com/rcyc/LambdaHandler.java
@@ -1,3 +1,5 @@
+package com.rcyc;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
